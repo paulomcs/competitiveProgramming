2e822523ca0159275ee1868a29efcf8fcb5fce27
Paulo Mateus Cunha Sampaio <pauloSC@seed.se.gov>
2023-09-06 11:22:43 -0300

    nothing much in bitmap

---
+++ TheHuxley/bitmap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main() {
+
+    int cicles, mAmnt, gAmnt;
+    scanf("%d", &cicles);
+
+    for(int i = 0; i < cicles; i++){
+        scanf("%d %d", &mAmnt, &gAmnt);
+        char Arr[mAmnt];
+        scanf("%s", m);
+    }
+
+    return 0;
+}
